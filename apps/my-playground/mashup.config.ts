a6c32554d0fb8751dabefac613f246525cb20efc
장현봉 <wkdgusqhd080@naver.com>
2025-04-17 23:53:22 +0900

    deploy: update cli packages version

--- apps/my-playground/mashup.config.ts
+++ apps/my-playground/mashup.config.ts
@@ -1,21 +1,21 @@
-import { MashupConfig } from "@mash-up-web-toolkit/command";
+import { MashupConfig } from '@mash-up-web-toolkit/command';
 
 const config: MashupConfig = {
   "gen:api": {
     /**
-     * 생성될 파일의 경로
+     * @description 생성될 파일의 경로
      */
-    output: "./src/__generated__",
+    output: "./src/generated",
 
     /**
-     * 생성할 API의 주소
+     * @description 생성할 API의 주소
      */
     url: "https://petstore.swagger.io/v2/swagger.json",
 
     /**
-     * fetch 또는 axios 인스턴스 경로
+     * @description fetch 또는 axios 인스턴스 경로
      */
-    instancePath: "@/configs/fetch/instance",
+    instancePath: "@/configs/axios/instance",
   },
 };
 
