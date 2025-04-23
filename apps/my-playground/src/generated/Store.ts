9b02067fbac01761d63a587def465c47aa9fd13c
장현봉 <wkdgusqhd080@naver.com>
2025-04-23 23:58:30 +0900

    feat: generated api write fn implement

---
+++ apps/my-playground/src/generated/Store.ts
@@ -0,0 +1,82 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+import instance from "@/configs/axios/instance";
+import { OrderType } from "./data-contracts";
+import { ContentType, HttpClient, RequestParams } from "./http-client";
+
+export class StoreApi<SecurityDataType = unknown> extends HttpClient<SecurityDataType> {
+  /**
+   * @description Returns a map of status codes to quantities
+   *
+   * @tags store
+   * @name GetInventory
+   * @summary Returns pet inventories by status
+   * @request GET:/store/inventory
+   * @secure
+   */
+  getInventory = (variables?: { params?: RequestParams }) =>
+    this.request<Record<string, number>, any>({
+      path: `/store/inventory`,
+      method: "GET",
+      secure: true,
+      format: "json",
+      ...variables?.params,
+    });
+  /**
+   * No description
+   *
+   * @tags store
+   * @name PlaceOrder
+   * @summary Place an order for a pet
+   * @request POST:/store/order
+   */
+  placeOrder = (variables: { body: OrderType; params?: RequestParams }) =>
+    this.request<OrderType, void>({
+      path: `/store/order`,
+      method: "POST",
+      body: variables.body,
+      type: ContentType.Json,
+      format: "json",
+      ...variables.params,
+    });
+  /**
+   * @description For valid response try integer IDs with value >= 1 and <= 10. Other values will generated exceptions
+   *
+   * @tags store
+   * @name GetOrderById
+   * @summary Find purchase order by ID
+   * @request GET:/store/order/{orderId}
+   */
+  getOrderById = (variables: { orderId: number; params?: RequestParams }) =>
+    this.request<OrderType, void>({
+      path: `/store/order/${variables.orderId}`,
+      method: "GET",
+      format: "json",
+      ...variables.params,
+    });
+  /**
+   * @description For valid response try integer IDs with positive integer value. Negative or non-integer values will generate API errors
+   *
+   * @tags store
+   * @name DeleteOrder
+   * @summary Delete purchase order by ID
+   * @request DELETE:/store/order/{orderId}
+   */
+  deleteOrder = (variables: { orderId: number; params?: RequestParams }) =>
+    this.request<any, void>({
+      path: `/store/order/${variables.orderId}`,
+      method: "DELETE",
+      ...variables.params,
+    });
+}
+
+export const storeApi = new StoreApi({ instance });
--- packages/cli/generate-api/src/generator/generator.ts
+++ packages/cli/generate-api/src/generator/generator.ts
@@ -1,4 +1,10 @@
-import { generateApi, GenerateApiParams } from "swagger-typescript-api";
+import {
+  generateApi,
+  GenerateApiOutput,
+  GenerateApiParams,
+} from "swagger-typescript-api";
+import path from "node:path";
+import fs from "node:fs";
 
 export const generateSwaggerApi = async (params: GenerateApiParams) => {
   return await generateApi(params)
@@ -11,3 +17,53 @@ export const generateSwaggerApi = async (params: GenerateApiParams) => {
       process.exit(1);
     });
 };
+
+export const writeGeneratedApi = (
+  result: GenerateApiOutput,
+  outputPath: string
+) => {
+  const { files } = result;
+  files.forEach((element) => {
+    const { fileName, fileContent } = element;
+    const folderPath = getFolderPath(outputPath, fileName);
+
+    fs.mkdirSync(folderPath, { recursive: true });
+    if (fileName === "http-client" || fileName === "data-contracts") {
+      createFile(path.resolve(folderPath, "index.ts"), fileContent);
+      return;
+    }
+    createFile(path.resolve(folderPath, `${fileName}.api.ts`), fileContent);
+  });
+};
+
+const getFolderPath = (outputPath: string, name: string) => {
+  switch (name) {
+    case "http-client": {
+      return path.resolve(outputPath, "@http-client");
+    }
+    case "data-contracts": {
+      return path.resolve(outputPath, "@types");
+    }
+    default:
+      return path.resolve(outputPath, name);
+  }
+};
+
+const createFile = (path: string, content: string) => {
+  const contentWithComment = `${commentTemplate}${content}`;
+  fs.writeFileSync(path, contentWithComment);
+};
+
+const commentTemplate = `/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+
+`;
