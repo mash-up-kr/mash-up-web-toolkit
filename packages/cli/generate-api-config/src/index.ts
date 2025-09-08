a7af0e7ba6b0d9a4599a27c7cdcd265da61a0373
장현봉 <brightbong92@amorepacific.com>
2025-09-08 17:30:22 +0900

    feat: API 제너레이터 설정파일생성 플러그인 추가

---
+++ packages/cli/generate-api-config/src/index.ts
@@ -0,0 +1,32 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
+import { axiosTemplate, fetchTemplate } from './config/template.js';
+
+export const initHttpClientConfig = (httpClientType: 'fetch' | 'axios') => {
+  const outputPath: string = './src/configs/__generated__';
+
+  const configPath = path.resolve(
+    process.cwd(),
+    `${outputPath}/${httpClientType}`,
+    `instance.ts`
+  );
+
+  const dirPath = path.dirname(configPath);
+
+  try {
+    fs.mkdirSync(dirPath, { recursive: true });
+  } catch (error) {
+    console.error(`❌ 디렉토리 생성 실패: ${dirPath}`, error);
+    return;
+  }
+
+  const template = httpClientType === 'fetch' ? fetchTemplate : axiosTemplate;
+
+  try {
+    fs.writeFileSync(configPath, template, 'utf-8');
+    console.log('✅ instance.ts 파일 생성 완료!');
+  } catch (error) {
+    console.error('❌ instance.ts 파일 생성 중 오류:', error);
+  }
+};
