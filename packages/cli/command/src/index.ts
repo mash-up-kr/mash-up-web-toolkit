ab2da9a315e1bc7e9f7b3d82974ac230e7ba2351
장현봉 <brightbong92@amorepacific.com>
2025-09-09 09:34:36 +0900

    fix: CLI 메인함수 구조 변경

--- packages/cli/command/src/index.ts
+++ packages/cli/command/src/index.ts
@@ -10,32 +10,22 @@ export type { MashupConfig } from './types/types.js';
 
 const controller = new Controller();
 
-export const welcome = () => {
-  console.log(figlet.textSync('Mash-Up Web CLI'));
-};
-
-export const runPrompt = async () => {
-  const choices = ['gen:config', 'gen:api-config', 'gen:api'];
-
-  inquirer
-    .prompt([
-      {
-        name: 'command',
-        type: 'list',
-        message: '명령을 선택해주세요.',
-        choices,
-        default: choices[0],
-      },
-    ])
-    .then(answers => {
-      commandFactory.executeCommand(answers.command, controller);
-    });
-};
-
 export const main = async () => {
   welcome();
 
   const program = new Command();
+  setupCliCommands(program);
+
+  if (process.argv.length <= 2) {
+    runInteractiveMode();
+  } else {
+    program.parse(process.argv);
+  }
+};
+
+main();
+
+export const setupCliCommands = (program: Command) => {
   program
     .name('mash-up')
     .description('Mash-Up Web Toolkit CLI')
@@ -64,16 +54,35 @@ export const main = async () => {
   program
     .command('gen:api-config')
     .description('API instance 파일을 생성합니다')
+    .option(
+      '-t, --type <type>',
+      'HTTP 클라이언트 타입 (fetch 또는 axios)',
+      'fetch'
+    )
     .action(async options => {
       const httpClientType = options.type as 'fetch' | 'axios';
       await controller.initApiConfig({ httpClientType });
     });
-
-  if (process.argv.length <= 2) {
-    runPrompt();
-  } else {
-    program.parse(process.argv);
-  }
 };
 
-main();
+export const runInteractiveMode = async () => {
+  const choices = ['gen:config', 'gen:api-config', 'gen:api'];
+
+  inquirer
+    .prompt([
+      {
+        name: 'command',
+        type: 'list',
+        message: '명령을 선택해주세요.',
+        choices,
+        default: choices[0],
+      },
+    ])
+    .then(answers => {
+      commandFactory.executeCommand(answers.command, controller);
+    });
+};
+
+export const welcome = () => {
+  console.log(figlet.textSync('Mash-Up Web CLI'));
+};
